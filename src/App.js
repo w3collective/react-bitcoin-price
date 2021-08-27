d359922bbd78ede783042e4dece78438344972f3
W3Collective <contact@w3collective.com>
2021-08-27 15:43:00 +1000

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import BitcoinPrice from './BitcoinPrice';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BitcoinPrice />
+    </div>
+  );
+}
+
+export default App;
